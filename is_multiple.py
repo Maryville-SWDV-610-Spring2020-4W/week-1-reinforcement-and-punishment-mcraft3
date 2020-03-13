21032a6bf9ae49ed49c5e11819476dbd3217fe98
Mike Craft <mcraft3@live.maryville.edu>
2020-03-13 15:40:04 -0400

    Update is_multiple.py
    Added protection for divide by zero on the second entry m. split main to add a getInput function.
    
    Entry of 0 for n is still allowed as n = mi which could be 0 = m*0 and n could be = i. Not sure I like that and it would seem better to not allow, but seems the definition supports it so trying to follow the definition. Glad to update and prevent 0 if that is not the intent. Please let me know.

--- is_multiple.py
+++ is_multiple.py
@@ -1,40 +1,40 @@
-# is_mutiple.py
-# SWDV-610-4W DATA STRUCTURES: WK 1 - Module 1.9
-#     Reinforcement and Punishment
-# Maryville University of St. Louis, MO
-# John E. Simon School of Business
-# Professor Timothy Kyle
-# Student Mike Craft
+"""is_mutiple.py
+SWDV-610-4W DATA STRUCTURES: WK 1 - Module 1.9
+   Reinforcement and Punishment
+Maryville University of St. Louis, MO
+John E. Simon School of Business
+Professor Timothy Kyle
+Student Mike Craft"""
 # ------------------------------------------------
-# Write a short Python function, is_multiple(n,
-# m), that takes two integer values and returns
-# True is n is a multiple of m, that is, n = mi
-# for some integer i, and False otherwise.
+"""# Write a short Python function, is_multiple(n,
+m), that takes two integer values and returns
+True is n is a multiple of m, that is, n = mi
+for some integer i, and False otherwise."""
 # ------------------------------------------------
 # GitHub Link: https://classroom.github.com/a/nUSsBI1q
 # ------------------------------------------------
-# In addition to coding these tasks, you must post
-# a video running and explaining your code.  This
-# allows for you to demonstrate what is occurring
-# in the code as it is happening and how it is
-# organized.  You must also run your code in the
-# video to explain the output and why the program
-# produced that output. Refer to the attached
-# rubric to see how it is scored.
+"""In addition to coding these tasks, you must
+post a video running and explaining your code.
+This allows for you to demonstrate what is
+occurring in the code as it is happening and how
+it is organized.  You must also run your code in
+the video to explain the output and why the
+program produced that output. Refer to the
+attached rubric to see how it is scored."""
 # ------------------------------------------------
-# Rubric
-# 1. Multiple Readability - Your code should have
-#      clearly articulated comments, easy to
-#      understand variables, constants, etc., and
-#      have a clear flow of how the code is
-#      carried out.
-# 2. Multiple Computation - Code should be able to
-#      run and carry out the necessary tasks. Aim
-#      to have your code complete the required
-#      tasks.
-# 3. Multiple Output - Returns the proper output
+"""Rubric
+1. Multiple Readability - Your code should have
+     clearly articulated comments, easy to
+     understand variables, constants, etc., and
+     have a clear flow of how the code is
+     carried out.
+2. Multiple Computation - Code should be able to
+     run and carry out the necessary tasks. Aim
+     to have your code complete the required
+     tasks.
+3. Multiple Output - Returns the proper output"""
 # ------------------------------------------------
-# Answer 3 canvas questions in the canvas
+"""Answer 3 canvas questions in the canvas
 #    submission text box.
 
 # 1. How many hours do you estimate you used
@@ -45,59 +45,84 @@
 
 # 3. What was the most difficult challenge you
 #    experienced when completing this assignment
+"""
 # ------------------------------------------------
-# Python function, is_multiple(n, m),
-#   that takes two integer values and returns True
-#   is n is a multiple of m, that is,
+"""Python function, is_multiple(n, m),
+that takes two integer values and returns True
+
+n = mi for some integer i,
+and False otherwise."""
+
+def is_multiple(n,m): # called by main function, variables as inputs.
+# call from main with variables n,m
+#   boolean True if n is / m and remainder is
+#   equal to 0 (modulo or remainder is 0). This
+#   also means n is a multiple of m, that is,
 #   n = mi for some integer i,
 #   and False otherwise.
 
-def is_multiple(n,m): # call from main with variables n,m
-# boolean True if n is / m and remainder is equal to 0
-#   (modulo or remainder is 0). This also means
-#   n is a multiple of m, that is, n = mi for some
-#   integer i, and False otherwise.
-
     if n % m == 0: 
-        print(n, "is a multiple of ", m)
+        print()
+        print(n, "is a multiple of", m)
     else:
-        print(n, "is not a multiple of ", m)
+        print()
+        print(n, "is not a multiple of", m)
 
-def main():
-# print introduction
-    print("""This program takes two integer values
-as inputs and outputs to screen (if True) that
-integer 1 is a multiple of integer 2, or will
-output to screen (if False) that integer 1 is not
-a multiple of integer 2.
-
-If either number is not an integer (text or symbol,
-or is a decimal number, you will get and error and
-will try again to enter that integer.""")
-    print()
-    
-    # validation loop 1 to catch n errors. Will only
-    #   allow an int entry; str or float excepted.
+def getInput(): # called by main.
+# validation loop 1 to catch n errors. Will only
+#   allow an int entry; str or float excepted.
     while True:
         try:
-            n = int(input("Enter integer number 1: "))
-        except ValueError:
-            print("ERROR: Entry must be an integer. Try again.")
+            n = int(input("Enter integer n: "))
+        except ValueError: # exception error here to block system error and stop / crash program.
+            print("\nValueError: Entry must be an integer (not a decimal, letter, or non whole number). Try again.\n")
             continue # loop again if error
-        else: # if an int break out of loop
+        else: # if an int (validated good entry) break out of loop
             break
             
-    # validation loop 2 to catch m errors. Will only
-    #   allow an int entry; str or float excepted.
+# validation loop 2 to catch m errors.
+# First checks for divide by zero here so it doesn't show up on line 64 from modulo.
+# Second checks for whole number integer, not decimal, or letter, or non whole number character.
+# Will only allow an int entry; str or float excepted.
     while True:
         try:
-            m = int(input("Enter integer number 2: "))
-        except ValueError:
+            m = int(input("Enter integer m: ")) # get input, set to variable m
+            if m == 0: # boolean if true (they entered 0, try again.)
+                print("\nZeroDivisionError: can not divide by zero, 2nd number can not be zero. Try again.\n")
+                continue # loop again if error
+            
+        except ValueError: # exception error here to block system error and stop / crash program.
             print("ERROR: entry must be an integer. Try again.")
             continue # loop again if error
-        else: # if an int break out of loop
+        
+        
+        else: # if an int (validated good entry) break out of loop
             break
         
-    is_multiple(n,m) # call the function with variables n,m
+    return n, m    # return the variables to the function that called (main).
+      
+def main():
+# print introduction
+    print("""
+This program takes two integer values (positive
+or negative whole number integers) as inputs and
+outputs to screen (if True) that integer n is a
+multiple of integer m (that is, n = mi for some
+integer i), or will output to screen (if False)
+that integer n is not a multiple of integer m.
+Under this definition, a 0 is allowed for n,
+and is considered a multiple of m as 0 = m*0.
 
+If either number is not an integer (text or
+symbol, or is a decimal number, you will get and
+error and will try again to enter that integer.
+
+If the second entry m is a 0, this program
+will show an error, and require the user to
+try again. A 0 in the second entry is an
+attempt to divide by zero and not allowed.""")
+    print()
+    n, m = getInput() # call the function to get inputs and return variables
+    is_multiple(n,m) # call the function with variables n,m
+    
 main()
